0f1f04d1dc7e50fed9031f473ffc83e840bbd8e3
PaviTechDevloper <pavithrasampath8925@gmail.com>
2026-05-26 00:31:30 +0530

    docker file change

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://api.medicojob.com';
-export const SOCKET_URL = 'http://api.medicojob.com';
+export const API_BASE_URL = '';
+export const SOCKET_URL = '';
 
 export default API_BASE_URL;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://api.medicojob.com';
-export const SOCKET_URL = 'http://api.medicojob.com';
+export const API_BASE_URL = '';
+export const SOCKET_URL = '';
