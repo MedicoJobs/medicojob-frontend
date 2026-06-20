4de26699f4e44f88f6b25ff8f712a49871b7415e
PaviTechDevloper <pavikutty0609@gmail.com>
2026-06-20 12:21:20 +0530

    fix: modify the pipeline

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,3 @@
-const hostname = globalThis?.location?.hostname || 'localhost';
-const API_BASE_URL = `http://${hostname}:5000`;
+import { API_BASE_URL } from './utils/api';
 
 export default API_BASE_URL;
