b2c016f4c2e3d6634cff807775cb197de0ad8ed5
PaviTechDevloper <pavikutty0609@gmail.com>
2026-06-22 11:07:54 +0530

    fix: use ingress origin for frontend api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const hostname = globalThis.location.hostname;
 const defaultApiUrl = hostname === 'localhost' || hostname === '127.0.0.1'
   ? 'http://localhost:5000'
-  : `http://${hostname}:5000`;
+  : globalThis.location.origin;
 
 const configuredApiUrl = process.env.REACT_APP_API_URL;
 
